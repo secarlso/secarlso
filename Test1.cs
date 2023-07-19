52cbc906b6363664fe281ab9f68fe0040d9efd3b
secarlso <44476966+secarlso@users.noreply.github.com>
2023-07-19 10:10:20 -0500

    Update Test1.cs

--- Test1.cs
+++ Test1.cs
@@ -1,1 +1,17 @@
-One
+using System;
+
+/// <summary>
+/// Summary description for Test1.cs
+/// </summary>
+public class Test1.cs
+{
+	public Test1.cs()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+  public void MyTestMethod()
+  {
+  }
+}
